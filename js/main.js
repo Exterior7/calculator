7621eeb9b868033345c673a0ed8056bcc6a2f2f8
Exterior7 <exteriorbwin@gmail.com>
2020-12-02 11:55:13 +0100

    temporary solution

--- js/main.js
+++ js/main.js
@@ -1,6 +1,9 @@
 function insert(num) {
     document.form.textview.value = document.form.textview.value+num
 }
+function clean() {
+    document.form.textview.value = "";
+}
 
 function equal() {
     let exp = document.form.textview.value;
@@ -9,7 +12,19 @@ function equal() {
     }
 }
 
-function clean() {
-    document.form.textview.value = "";
+
+
+const clacs = {
+    '+': function(a, b) {return a + b},
+    '-': function(a, b) {return a - b},
+    '/': function(a, b) {return a / b},
+    '*': function(a, b) {return a * b}
 }
 
+// const signs = ['+', '-', '*', '/'];
+
+// for (let i = 0; i < signs.length; i += 1) {
+//    if (i = i + 1) {}
+// }
+
+// const acceptableSings = ['+', '-', '*', '/']
